ddab12f9a3b7856f4f94118042ef9e59f96bdccc
Altures <antoniomanduca15@gmail.com>
2025-05-29 14:30:10 -0300

    Projeto final
    Aqui toda a ideia foi mudada, adicionei perguntas, mudei o tema e o resto
    o tema foi alterado para BTS.
    todas as perguntas foram refeitas.
    o plano de fundo foi mudado.
    as cores dos cartões também foram alteradas

---
+++ flashcard final/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Factual',
+    'Quantos integrantes tem o BTS?',
+    'Sete.'
+)
+
+criaCartao(
+    'Temporal',
+    'Em que ano o BTS foi formado?',
+    '2013.'
+)
+
+criaCartao(
+    'Pessoal',
+    'Qual membro do BTS se chama Kim Namjoon?',
+    'RM'
+)
+
+criaCartao(
+    'Geográfica',
+    'De qual país o BTS é?',
+    'Coreia do Sul.'
+)
+
+criaCartao(
+    'Causal',
+    'Por que o BTS lançou "Dynamite" em inglês?',
+    'Para alcançar um público global durante a pandemia.'
+)
+
+criaCartao(
+    'Comparativa',
+    'Quem é mais velho: Jimin ou Jin?',
+    'Jin.'
+)
+
+criaCartao(
+    'Sequencial',
+    'Qual foi o primeiro single do BTS?',
+    'No More Dream.'
+)
+
+criaCartao(
+    'De definição',
+    'O que significa BTS?',
+    'Bangtan Sonyeondan (Bulletproof Boy Scouts).'
+)
+
+criaCartao(
+    'De opinião',
+    'Qual álbum do BTS é considerado o mais emocional por muitos fãs?',
+    'The Most Beautiful Moment in Life: Young Forever.'
+)
+
